f1aff4db13b0a8e9feafb0ede741e031ecf6e7c0
invinciboll <boll.basti@gmail.com>
2025-02-09 16:22:02 +0100

    Adjust to ssh

--- frontend/src/Envs.ts
+++ frontend/src/Envs.ts
@@ -1,5 +1,5 @@
 export const backendUrl =
     import.meta.env.VITE_BACKEND_HOST && import.meta.env.VITE_BACKEND_PORT
-        ? `https://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT
+        ? `https://${import.meta.env.VITE_BACKEND_HOST
         }`
         : "http://localhost:4711";
